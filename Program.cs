d9579716451d320be84c75345e51c9c856fac86a
Maxw_ll <ma.bilang0023@gmail.com>
2025-12-12 14:25:43 +0000

    Reading File
    Using https://learn.microsoft.com/en-us/troubleshoot/developer/visualstudio/csharp/language-compilers/read-write-text-file

--- Program.cs
+++ Program.cs
@@ -11,13 +11,33 @@ namespace AoCdayone
     {
         static void Main(string[] args)
         {
-            int start = 50;
-
-            string filePath = @"C:\MaxwellBilango\CSharpCode\AoC2025\DayonePI.txt";
-            List<char> letter = new List<char>();
-
-
-            Console.WriteLine("Reading the file line by line:");
+            String line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("C:\\Sample.txt");
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //write the line to console window
+                    Console.WriteLine(line);
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+                //close the file
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
         }
     }
 }
