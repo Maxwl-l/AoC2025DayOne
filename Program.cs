7efa66d58608808cdca4f300298a9b707bd61566
Maxw_ll <ma.bilang0023@gmail.com>
2025-12-12 14:01:33 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoCdayone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 50;
+
+            string filePath = @"C:\MaxwellBilango\CSharpCode\AoC2025\DayonePI.txt";
+            List<char> letter = new List<char>();
+
+            using (var reader = new StreamReader(filePath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    int ch = reader.Read();
+                    Console.Write((char)ch);
+                    string begin = (ch).SubString(0, 1);
+                    
+                    
+                    
+                }
+                Console.Write(letter[0]);
+                Console.Write(letter[1]);
+            }
+        }
+    }
+}
