6ae8f6aae8c97093429a1408c0774651990487fb
Maxw_ll <ma.bilang0023@gmail.com>
2025-12-15 14:25:31 +0000

    attempting to check errors manually

--- Program.cs
+++ Program.cs
@@ -12,20 +12,69 @@ namespace AoCdayone
         static void Main(string[] args)
         {
             String line;
+            string sub;
+            int amount;
+            int starting = 50;
+            int zero = 0;
+            int temp;
+            int temp2;
+
             try
             {
                 //Pass the file path and file name to the StreamReader constructor
-                StreamReader sr = new StreamReader("C:\\Sample.txt");
+                StreamReader sr = new StreamReader("C:/MaxwellBilango/CSharpCode/AoC2025/DayonePI.txt");
                 //Read the first line of text
                 line = sr.ReadLine();
                 //Continue to read until you reach end of file
+                Console.WriteLine("START");
                 while (line != null)
                 {
                     //write the line to console window
-                    Console.WriteLine(line);
+                    
+                    sub = line.Substring(0, 1);
+                    amount = Int32.Parse(line.Substring(1));
+                    Console.WriteLine(starting);
+                    if (sub == "L")
+                    {
+                        starting = starting - amount;
+                        
+                        if (starting < 0)
+                        {
+                            zero = zero + 1;
+                            temp = starting * -1;
+                            starting = 99 - (temp - 1);
+                            
+                        }
+                        else if (starting == 0)
+                        {
+                            zero = zero + 1;
+                        }
+                        
+                    }
+                    else if (sub == "R")
+                    {
+                        starting = starting + amount;
+                        
+                        if (starting > 99)
+                        {
+                            zero = zero + 1;
+                            temp = starting - 99;
+                            starting = 0 + (temp - 1);
+                            
+                        }
+                        else if (starting == 0)
+                        {
+                            zero = zero + 1;
+                        }
+                        
+                    }
+
                     //Read the next line
                     line = sr.ReadLine();
                 }
+
+                Console.WriteLine(zero);
+
                 //close the file
                 sr.Close();
                 Console.ReadLine();
