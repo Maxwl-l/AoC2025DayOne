e481be4e1e349770cca566a15c86f2b77a29dde8
Maxw_ll <ma.bilang0023@gmail.com>
2025-12-15 15:35:35 +0000

    Final answer using chatgpt
    Completely forgot about %, probably would have saved me a lot of time.
    Used chat to help figure out why my code wasn't correct.

--- Program.cs
+++ Program.cs
@@ -9,74 +9,168 @@ namespace AoCdayone
 {
     internal class Program
     {
+        //static void Main(string[] args)
+        //{
+        //    String line;
+        //    string sub;
+        //    int amount;
+        //    int starting = 50;
+        //    int zero = 0;
+        //    int temp;
+        //    int temp2;
+
+        //    try
+        //    {
+        //        //Pass the file path and file name to the StreamReader constructor
+        //        StreamReader sr = new StreamReader("C:/MaxwellBilango/CSharpCode/AoC2025/DayonePI.txt");
+        //        //Read the first line of text
+        //        line = sr.ReadLine();
+        //        //Continue to read until you reach end of file
+        //        Console.WriteLine("START");
+        //        while (line != null)
+        //        {
+        //            //write the line to console window
+
+        //            sub = line.Substring(0, 1);
+        //            amount = Int32.Parse(line.Substring(1));
+
+        //            if (sub == "R")
+        //            {
+        //                temp2 = starting + amount;
+        //                if (temp2 > 99)
+        //                {
+        //                    temp = temp2 - 99;
+        //                    temp2 = 0 + (temp - 1);
+        //                    if (temp2 > 99)
+        //                    {
+        //                        temp = temp2 - 99;
+        //                        temp2 = 0 + (temp - 1);
+        //                    }
+        //                    Console.WriteLine(temp2);
+        //                    starting = temp2;
+        //                    if (starting == 0)
+        //                    {
+        //                        zero = zero + 1;
+        //                    }
+
+        //                }
+        //                else if (temp2 == 0)
+        //                {
+        //                    zero = zero + 1;
+        //                    Console.WriteLine(temp2);
+        //                    starting = temp2;
+
+        //                }
+        //                else
+        //                {
+        //                    Console.WriteLine(temp2);
+        //                    starting = temp2;
+        //                }
+
+        //            }
+
+
+        //            if (sub == "L")
+        //            {
+        //                temp2 = starting - amount;
+        //                if (temp2 < 0)
+        //                {
+        //                    temp = temp2 * -1;
+        //                    temp2 = 100 - (temp);
+        //                    if (temp < 0)
+        //                    {
+        //                        temp = temp2 * -1;
+        //                        temp2 = 100 - (temp);
+        //                    }
+        //                    Console.WriteLine(temp2);
+        //                    starting = temp2;
+        //                    if (starting == 0)
+        //                    {
+        //                        zero = zero + 1;
+        //                    }
+        //                }
+        //                else if (temp2 == 0)
+        //                {
+        //                    zero = zero + 1;
+        //                    Console.WriteLine(temp2);
+        //                    starting = temp2;
+
+        //                }
+        //                else
+        //                {
+        //                    Console.WriteLine(temp2);
+        //                    starting = temp2;
+
+        //                }
+
+        //            }
+        //            Console.WriteLine("-----------------");
+        //            //Read the next line
+        //            line = sr.ReadLine();
+        //        }
+
+        //        Console.WriteLine("Answer:");
+        //        Console.WriteLine(zero);
+
+        //        //close the file
+        //        sr.Close();
+        //        Console.ReadLine();
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine("Exception: " + e.Message);
+        //    }
+        //    finally
+        //    {
+        //        Console.WriteLine("Executing finally block.");
+        //    }
+        //}
+
         static void Main(string[] args)
         {
-            String line;
-            string sub;
-            int amount;
+            string line;
             int starting = 50;
             int zero = 0;
-            int temp;
-            int temp2;
 
             try
             {
-                //Pass the file path and file name to the StreamReader constructor
-                StreamReader sr = new StreamReader("C:/MaxwellBilango/CSharpCode/AoC2025/DayonePI.txt");
-                //Read the first line of text
-                line = sr.ReadLine();
-                //Continue to read until you reach end of file
+                StreamReader sr = new StreamReader(
+                    "C:/MaxwellBilango/CSharpCode/AoC2025/DayonePI.txt"
+                );
+
                 Console.WriteLine("START");
-                while (line != null)
+
+                while ((line = sr.ReadLine()) != null)
                 {
-                    //write the line to console window
-                    
-                    sub = line.Substring(0, 1);
-                    amount = Int32.Parse(line.Substring(1));
-                    Console.WriteLine(starting);
-                    if (sub == "L")
+                    char direction = line[0];
+                    int amount = int.Parse(line.Substring(1));
+
+                    // Reduce rotation to within 0–99
+                    amount %= 100;
+
+                    if (direction == 'R')
                     {
-                        starting = starting - amount;
-                        
-                        if (starting < 0)
-                        {
-                            zero = zero + 1;
-                            temp = starting * -1;
-                            starting = 99 - (temp - 1);
-                            
-                        }
-                        else if (starting == 0)
-                        {
-                            zero = zero + 1;
-                        }
-                        
+                        starting = (starting + amount) % 100;
                     }
-                    else if (sub == "R")
+                    else if (direction == 'L')
                     {
-                        starting = starting + amount;
-                        
-                        if (starting > 99)
-                        {
-                            zero = zero + 1;
-                            temp = starting - 99;
-                            starting = 0 + (temp - 1);
-                            
-                        }
-                        else if (starting == 0)
-                        {
-                            zero = zero + 1;
-                        }
-                        
+                        starting = (starting - amount + 100) % 100;
                     }
 
-                    //Read the next line
-                    line = sr.ReadLine();
+                    Console.WriteLine(starting);
+
+                    if (starting == 0)
+                    {
+                        zero++;
+                    }
+
+                    Console.WriteLine("-----------------");
                 }
 
-                Console.WriteLine(zero);
-
-                //close the file
                 sr.Close();
+
+                Console.WriteLine("Answer:");
+                Console.WriteLine(zero);
                 Console.ReadLine();
             }
             catch (Exception e)
@@ -89,4 +183,4 @@ namespace AoCdayone
             }
         }
     }
-}
+    }
